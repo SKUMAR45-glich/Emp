bd7b58d1170cffb60779c7e39a8c7d60920cbe5d
ABC <abc@gmail.com>
2020-09-29 14:12:21 +0530

    start1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿uusing System;
+
+namespace Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
+        }
+    }
+}
