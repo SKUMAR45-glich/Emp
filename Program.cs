17ef44918d61dc82a8c82770693cb79c61741c64
ABC <abc@gmail.com>
2020-09-29 14:23:26 +0530

    UC 2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿uusing System;
+﻿using System;
 
 namespace Employee
 {
@@ -6,19 +6,23 @@ namespace Employee
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
             //Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee id Present");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage:" + empWage);
         }
     }
 }
