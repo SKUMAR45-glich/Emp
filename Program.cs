cbf8f5446f5840435cdc341d8960aa2c01842075
SKUMAR45-glich <71806111+SKUMAR45-glich@users.noreply.github.com>
2020-10-01 03:53:17 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,13 +8,13 @@ namespace Employee
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 2;
-        public const int MAX_HRS_IN_NORTH = 10;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_<MONTH = 100;
         static void int EmpWage()
         {
             //Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
             int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-            while (totalEmpHrs <= MAX_HRS_IN_NORTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
                 totalWorkingDays++;
                 Random random = new Random();
