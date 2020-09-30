0f387fb52f4e568d5fc9c6b0dcc1acdd5ef8483b
SKUMAR45-glich <71806111+SKUMAR45-glich@users.noreply.github.com>
2020-10-01 03:54:41 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace Employee
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 2;
+        public const int NUM_OF_WORKING_DAYS = 20;
         static void Main(string[] args)
         {
             //Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
