4e383bb2ac7cbc0b0e561b6a7ad98f6236d3153c
Saurabh <saurabh.nit20@gmail.com>
2020-10-02 03:17:04 +0530

    UC 12

--- Program.cs
+++ Program.cs
@@ -4,6 +4,12 @@ using System.Collections.Generic;
 using System.Diagnostics; 
 namespace Employee
 {
+    public interface IComputeEmpWage
+    {
+        public void addCompanyEmpWage(string company, int empRatePerHour, int numofWorkingDays, int maxHoursPerMonth);
+        public void computeEmpWage();
+        public int getTotalWage(string company);
+    }
     public class CompanyEmpWage
     {
         public string company;
@@ -11,30 +17,92 @@ namespace Employee
         public int numofWorkingDays;
         public int maxHoursPerMonth;
         public int totalEmpWage;
-        public CompanyEmpWage(string company, int empRatePerHour, int numofWorkingDays, int maxHoursPerMonth, int totalEmpWage)
+        public CompanyEmpWage(string company, int empRatePerHour, int numofWorkingDays, int maxHoursPerMonth)
         {
             this.company = company;
             this.empRatePerHour = empRatePerHour;
             this.numofWorkingDays = numofWorkingDays;
             this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+        public void setTotalEmpWage(int totalEmpWage)
+        {
             this.totalEmpWage = totalEmpWage;
         }
+        public string toString()
+        {
+            return "Total Emp Wage for company:" + this.company + "is" + this.totalEmpWage;
+        }
     }
-       
+    public class EmpWageBuilder: IComputeEmpWage
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        //private int numofCompany = 0;
+        private LinkedList<CompanyEmpWage> companyEmpWagesList;
+        private Dictionary<string, CompanyEmpWage> companyToEmpWageMap;
+
+        public EmpWageBuilder()
+        {
+            this.companyEmpWagesList = new LinkedList<CompanyEmpWage>();
+            this.companyToEmpWageMap = new Dictionary<string, CompanyEmpWage>();
+        }
+        public void addCompanyEmpWage(string company, int empRatePerHour, int numofWorkingDays, int maxHoursPerMonth)
+        {
+            CompanyEmpWage companyEmpWage = new CompanyEmpWage(company, empRatePerHour, numofWorkingDays, maxHoursPerMonth);
+            this.companyEmpWagesList.AddLast(companyEmpWage);
+            this.companyToEmpWageMap.Add(company, companyEmpWage);
+        }
+        public void computeEmpWage()
+        {
+            foreach(CompanyEmpWage companyEmpWage in this.companyEmpWagesList)
+            {
+                companyEmpWage.setTotalEmpWage(this.computeEmpWage(companyEmpWage));
+                Console.WriteLine(companyEmpWage.toString());
+            }
+
+        }
+        private int computeEmpWage(CompanyEmpWage companyEmpWage)
+        {
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while (totalEmpHrs <= companyEmpWage.maxHoursPerMonth && totalWorkingDays < companyEmpWage.numofWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Date:" + totalWorkingDays + "Emp Hrs:" + empHrs);
+            }
+            return totalEmpHrs * companyEmpWage.empRatePerHour;
+
+        }
+        public int getTotalWage(string company)
+        {
+            return this.companyToEmpWageMap[company].totalEmpWage;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            var arlist = new ArrayList();
-            arlist.Add(new CompanyEmpWage("abc", 101, 10, 13000,1200));
-            arlist.Add(new CompanyEmpWage("def", 101, 10, 13000,1200));
-            arlist.Add(new CompanyEmpWage("ghi", 101, 10, 13000,1200));
-            arlist.Add(new CompanyEmpWage("fgh", 101, 10, 13000,1200));
-            /*foreach (var item in arlist)
-            {
-                Console.Write(item + ", ");
-            }*/
-               
+            EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+            empWageBuilder.addCompanyEmpWage("abc", 20, 2, 20);
+            empWageBuilder.addCompanyEmpWage("def", 40, 1, 10);
+            empWageBuilder.computeEmpWage();
+            Console.WriteLine("Total wage of abc company: " + empWageBuilder.getTotalWage("abc"));
+
         }
     }
 }
